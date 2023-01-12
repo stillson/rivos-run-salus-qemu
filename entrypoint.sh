a9c5cd160bd3c78551a9c16209652eb06ad91159
Chris Stillson <stillson@rivosinc.com>
2023-01-12 10:50:53 -0800

    rootdir, not root

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 export QEMU_NCPU=$1
 export QEMU_MEM=$2
 export QEMU_TIMEOUT=$3
-export SALUS_ROOT=$GITHUB_WORKSPACE/salus
+export SALUS_ROOTDIR=$GITHUB_WORKSPACE/salus
 
 if /opt/scripts/overwatch.py
 then
